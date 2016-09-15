4ceef0454c6bfb50c85ceaa623c81def5b75647b
chrismclarke <chris.m.clarke@live.co.uk>
2016-09-15 13:11:04 +0100

    Working form open, local db cache and form.downloadable logic

--- app/pages/forms/forms.ts
+++ app/pages/forms/forms.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams, ViewController} from 'ionic-angular';
+import {DomSanitizationService, SafeResourceUrl} from '@angular/platform-browser'
 
 
 
@@ -9,15 +10,24 @@ import {NavController, NavParams, ViewController} from 'ionic-angular';
 
 export class FormsPage {
   form:any;
+  enketoLink:SafeResourceUrl;
 
-  constructor(private params:NavParams, public viewCtrl:ViewController) {
+  constructor(private params:NavParams, public viewCtrl:ViewController, private sanitizer:DomSanitizationService) {
     this.form=params.data.form;
+    //use sanitizer to avoid error unsafe resource url (trusted)
+    this.enketoLink=this.sanitizer.bypassSecurityTrustResourceUrl(this.form.enketoLink);
     console.log(this.form);
   }
   close(){
+    //attempts to retrieve info from loaded iframe .... not working - need to access iframe.contents which requires same domain (check on mobile?)
+   /* var iframe:any = document.getElementById('form-iframe');
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    console.log(innerDoc);
     let pending=document.getElementsByClassName('offline-enabled__queue-length');
     console.log(pending);
-    this.viewCtrl.dismiss('data')
+    this.viewCtrl.dismiss(pending)
+    */
+    this.viewCtrl.dismiss()
   }
 
 
--- app/pages/forms/overview/overview.ts
+++ app/pages/forms/overview/overview.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs/Observable'
 import {Modal} from "ionic-angular/index";
 import {ModalController} from "ionic-angular"
 import {FormsPage} from "../forms";
+import {Storage, SqlStorage} from 'ionic-angular'
 
 
 
@@ -14,51 +15,67 @@ import {FormsPage} from "../forms";
 export class OverviewPage {
   results: any = [];
   anyErrors: boolean;
-  finished: boolean;
+  finished: boolean=true;
+  forms:any;
   generating:boolean;
   enketoLink:any;
+  storage:Storage;
   private data: Observable<any>;
 
   constructor(private koboApi:KoboApi, public nav:NavController, public modal:ModalController) {
-    this.getResults();
+
+   this.storage = new Storage(SqlStorage);
+    this.storage.get('forms').then((forms)=>{
+      if(forms){this.forms=(JSON.parse(forms))}
+      else{this.finished=false;this.getResults()}
+      console.log(this.forms)}
+    )
+
 
   }
 
   getResults(){
+    this.anyErrors=false;
     this.koboApi.koboRequest('https://kc.kobotoolbox.org/api/v1/forms').subscribe(
-        result =>this.results = result,
-        error =>this.anyErrors=true,
-        () => this.finished = true
+        result =>this.forms = result,
+        error => {
+          console.log(error);
+          this.anyErrors = true;
+          this.finished = true;
+        },
+        () => {
+          this.finished = true;
+          let i=0;
+          this.storage.set('forms',JSON.stringify(this.forms));
+          for(let form of this.forms){
+            this.getLinks(form, i);
+            i++
+          }}
     );
   }
 
+  getLinks(form, index){
+    this.koboApi.koboRequest(form.url + '/enketo').subscribe(
+        //**need to also save link to cache
+        result =>{
+          this.forms[index].enketoLink = result['enketo_url'].replace('https://','http://')
+        },
+              error =>{console.log(error)},
+              () => {
+                this.storage.set('forms',JSON.stringify(this.forms));
+              })
+        }
+
   openForm(form) {
-    console.log(form);
-    if (!form.enketoLink) {
-      this.generating=true;
-      this.koboApi.koboRequest(form.url + '/enketo').subscribe(
-          result =>this.enketoLink = result,
-          error =>this.anyErrors = true,
-          () => {
-            console.log('finished');
-            this.showModal(form)
-          }
-      );
-
-    }
+    let modal = this.modal.create(FormsPage, {form: form}, {
+      showBackdrop: false,
+      enableBackdropDismiss: false
+    });
+    modal.onDidDismiss(data=> {
+      console.log(data)
+    });
+    modal.present();
   }
-
-  showModal(form){
-    console.log(form)
-      let modal = this.modal.create(FormsPage, {form: form}, {
-        showBackdrop: false,
-        enableBackdropDismiss: false
-      });
-      modal.onDidDismiss(data=> {
-        console.log(data)
-      });
-      modal.present();
-    }
 
 
   refresh(){
