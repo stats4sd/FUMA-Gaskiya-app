d313b2ff9c696a6aa1f1e405342c44aa4496f2d1
lawalist <lawalist@yahoo.fr>
2018-06-02 09:42:30 +0300

    Synchronisation fin attelier kenya

--- src/app/culture-essai.class.ts
+++
@@ -1,8 +0,0 @@
-export class CultureType {
-    culture: any = null;
-    variete: any = null;
-    superficie_essai: any = null;
-    superficie_controle: any = null;
-    variables_essai: any = [];
-    variables_controle: any = [];
-}
